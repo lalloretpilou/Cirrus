5279582907838d4ab411505bdaca6d70a7ab80cc
L'ALLORET Pierre-Louis <pierrelouis95lalloret@gmail.com>
2025-08-25 20:20:39 +0200

    Initial Commit

---
+++ Cirrus/CirrusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirrusApp.swift
+//  Cirrus
+//
+//  Created by Pierre-Louis L'ALLORET on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CirrusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
